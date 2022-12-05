bb59497bf216e0ac3e34ac2b9c05199814cb47b7
Sarita-rgb <85011585+Sarita-rgb@users.noreply.github.com>
2022-12-05 08:39:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents healthy new diner')
-streamlit.header('breakfast Menu')
-streamlit.text('"U+1F350"Omega 3 & blueberry oatmeal')
-streamlit.text('U+1F350	Kale,Spinach & rocket Smoothie')
-streamlit.text('U+1F350	Hard-Boiled Free -Range Egg')
+streamlit.header('breakfast favorites')
+streamlit.text('🍔 Omega 3 & blueberry oatmeal')
+streamlit.text('🌯	Kale,Spinach & rocket Smoothie')
+streamlit.text('🥗	Hard-Boiled Free -Range Egg')
