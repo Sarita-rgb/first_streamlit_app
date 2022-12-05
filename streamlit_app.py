45f6fd2da0dcb2a8d8593e003d0a97466c2269f3
Sarita-rgb <85011585+Sarita-rgb@users.noreply.github.com>
2022-12-05 08:45:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text('🌯	Kale,Spinach & rocket Smoothie')
 streamlit.text('🥗	Hard-Boiled Free -Range Egg')
 
 streamlit.header('🍜build your own fruit smoothie🍝')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
